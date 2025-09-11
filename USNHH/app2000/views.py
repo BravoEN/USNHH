b8b2860211ff7dd30d531e184211f07e56ab9f7c
BravoEN <eskilnilsen2004@gmail.com>
2025-09-11 14:44:20 +0200

    added templates and urls and views

--- USNHH/app2000/views.py
+++ USNHH/app2000/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Student, Ansatt, Emne, Forelesning
 
 # Create your views here.
+def index(request):
+    studenter = Student.objects.all()
+    context = {'studenter' : studenter}
+    return render(request, 'index.html', context)
