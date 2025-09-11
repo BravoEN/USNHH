2fa2442ab7a29fef3f24235f1de391a35ace0da3
BravoEN <eskilnilsen2004@gmail.com>
2025-09-11 12:23:32 +0200

    registrerte classes i admin panelet

--- USNHH/app2000/admin.py
+++ USNHH/app2000/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Ansatt, Emne, Student, Forelesning
 
 # Register your models here.
+admin.site.register(Ansatt)
+admin.site.register(Student)
+admin.site.register(Emne)
+admin.site.register(Forelesning)
